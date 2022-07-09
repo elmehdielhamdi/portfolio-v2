1b79556cea98242b3efcb949d7fb8918580a5787
El Mehdi El Hamdi <mehdi.hamdi.mail@gmail.com>
2022-07-09 02:27:22 +0100

    remove portfolio 1 and added weather app

--- src/assets/img/works/index.js
+++ src/assets/img/works/index.js
@@ -2,3 +2,4 @@ export { default as portfolioV2 } from "./portfolio-v2.webp";
 export { default as portfolioV1 } from "./portfolio-v1.webp";
 export { default as cssGenerator } from "./css-generator.webp";
 export { default as robot } from "./robot.webp";
+export { default as weather } from "./weather.jpeg";
